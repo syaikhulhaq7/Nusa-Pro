ba394f3aeee26dfafb7e632b64cb446b3d0ee812
syaikhulhaq7 <syaikhulhaq7@gmail.com>
2026-08-19 14:13:10 +0700

    Add Nusa Pro offline service worker

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='nusa-pro-v1';const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
